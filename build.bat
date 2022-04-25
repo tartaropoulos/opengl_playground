c27f93f14ad1c6528f1da82fca9bc10f74b6ebb0
tartaropoulos <rangel.stanev@gmail.com>
2022-04-25 20:56:55 +0300

    add build type and vcpkg triplet

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 
 mkdir build
 cd build
-cmake -G "MinGW Makefiles" .. || goto ERROR
+cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DVCPKG_TARGET_TRIPLET=x64-mingw-static .. || goto ERROR
 mingw32-make -j8 || goto ERROR
 :: mingw32-make install || goto ERROR
 
