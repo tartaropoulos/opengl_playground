33cc10375120603a0d23a9145fd0455fdc4f63b0
tartaropoulos <rangel.stanev@gmail.com>
2022-08-18 20:04:45 +0300

    update main

--- src/opengl_tutorial/lesson_5/main.cpp
+++ src/opengl_tutorial/lesson_5/main.cpp
@@ -1,4 +1,4 @@
-// http://www.opengl-tutorial.org/beginners-tutorials/tutorial-4-a-colored-cube/
+// http://www.opengl-tutorial.org/beginners-tutorials/tutorial-5-a-textured-cube/
 
 #include <iostream>
 #include <optional>
@@ -9,6 +9,7 @@
 #include <glm\gtc\type_ptr.hpp>
 
 #include "../common/shader.h"
+#include "../common/texture.h"
 
 int HEIGHT = 1024;
 int WIDTH  = 768;
@@ -62,42 +63,42 @@ int main()
     glBindVertexArray(vertexArrayID);
 
     static const GLfloat gVertexBufferData[] = {
-        -0.5f,-0.5f,-0.5f, // triangle 1 : begin
-        -0.5f,-0.5f, 0.5f,
-        -0.5f, 0.5f, 0.5f, // triangle 1 : end
-         0.5f, 0.5f,-0.5f, // triangle 2 : begin
-        -0.5f,-0.5f,-0.5f,
-        -0.5f, 0.5f,-0.5f, // triangle 2 : end
-         0.5f,-0.5f, 0.5f,
-        -0.5f,-0.5f,-0.5f,
-         0.5f,-0.5f,-0.5f,
-         0.5f, 0.5f,-0.5f,
-         0.5f,-0.5f,-0.5f,
-        -0.5f,-0.5f,-0.5f,
-        -0.5f,-0.5f,-0.5f,
-        -0.5f, 0.5f, 0.5f,
-        -0.5f, 0.5f,-0.5f,
-         0.5f,-0.5f, 0.5f,
-        -0.5f,-0.5f, 0.5f,
-        -0.5f,-0.5f,-0.5f,
-        -0.5f, 0.5f, 0.5f,
-        -0.5f,-0.5f, 0.5f,
-         0.5f,-0.5f, 0.5f,
-         0.5f, 0.5f, 0.5f,
-         0.5f,-0.5f,-0.5f,
-         0.5f, 0.5f,-0.5f,
-         0.5f,-0.5f,-0.5f,
-         0.5f, 0.5f, 0.5f,
-         0.5f,-0.5f, 0.5f,
-         0.5f, 0.5f, 0.5f,
-         0.5f, 0.5f,-0.5f,
-        -0.5f, 0.5f,-0.5f,
-         0.5f, 0.5f, 0.5f,
-        -0.5f, 0.5f,-0.5f,
-        -0.5f, 0.5f, 0.5f,
-         0.5f, 0.5f, 0.5f,
-        -0.5f, 0.5f, 0.5f,
-         0.5f,-0.5f, 0.5f
+        -1.0f,-1.0f,-1.0f,
+		-1.0f,-1.0f, 1.0f,
+		-1.0f, 1.0f, 1.0f,
+		 1.0f, 1.0f,-1.0f,
+		-1.0f,-1.0f,-1.0f,
+		-1.0f, 1.0f,-1.0f,
+		 1.0f,-1.0f, 1.0f,
+		-1.0f,-1.0f,-1.0f,
+		 1.0f,-1.0f,-1.0f,
+		 1.0f, 1.0f,-1.0f,
+		 1.0f,-1.0f,-1.0f,
+		-1.0f,-1.0f,-1.0f,
+		-1.0f,-1.0f,-1.0f,
+		-1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f,-1.0f,
+		 1.0f,-1.0f, 1.0f,
+		-1.0f,-1.0f, 1.0f,
+		-1.0f,-1.0f,-1.0f,
+		-1.0f, 1.0f, 1.0f,
+		-1.0f,-1.0f, 1.0f,
+		 1.0f,-1.0f, 1.0f,
+		 1.0f, 1.0f, 1.0f,
+		 1.0f,-1.0f,-1.0f,
+		 1.0f, 1.0f,-1.0f,
+		 1.0f,-1.0f,-1.0f,
+		 1.0f, 1.0f, 1.0f,
+		 1.0f,-1.0f, 1.0f,
+		 1.0f, 1.0f, 1.0f,
+		 1.0f, 1.0f,-1.0f,
+		-1.0f, 1.0f,-1.0f,
+		 1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f,-1.0f,
+		-1.0f, 1.0f, 1.0f,
+		 1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f, 1.0f,
+		 1.0f,-1.0f, 1.0f
     };
 
     // Настройка VBO
@@ -107,49 +108,49 @@ int main()
     // Перадеатся буффер в OpenGL
     glBufferData(GL_ARRAY_BUFFER, sizeof(gVertexBufferData), gVertexBufferData, GL_STATIC_DRAW);
 
-    static const GLfloat gColorBufferData[] = {
-        0.583f,  0.771f,  0.014f,
-        0.609f,  0.115f,  0.436f,
-        0.327f,  0.483f,  0.844f,
-        0.822f,  0.569f,  0.201f,
-        0.435f,  0.602f,  0.223f,
-        0.310f,  0.747f,  0.185f,
-        0.597f,  0.770f,  0.761f,
-        0.559f,  0.436f,  0.730f,
-        0.359f,  0.583f,  0.152f,
-        0.483f,  0.596f,  0.789f,
-        0.559f,  0.861f,  0.639f,
-        0.195f,  0.548f,  0.859f,
-        0.014f,  0.184f,  0.576f,
-        0.771f,  0.328f,  0.970f,
-        0.406f,  0.615f,  0.116f,
-        0.676f,  0.977f,  0.133f,
-        0.971f,  0.572f,  0.833f,
-        0.140f,  0.616f,  0.489f,
-        0.997f,  0.513f,  0.064f,
-        0.945f,  0.719f,  0.592f,
-        0.543f,  0.021f,  0.978f,
-        0.279f,  0.317f,  0.505f,
-        0.167f,  0.620f,  0.077f,
-        0.347f,  0.857f,  0.137f,
-        0.055f,  0.953f,  0.042f,
-        0.714f,  0.505f,  0.345f,
-        0.783f,  0.290f,  0.734f,
-        0.722f,  0.645f,  0.174f,
-        0.302f,  0.455f,  0.848f,
-        0.225f,  0.587f,  0.040f,
-        0.517f,  0.713f,  0.338f,
-        0.053f,  0.959f,  0.120f,
-        0.393f,  0.621f,  0.362f,
-        0.673f,  0.211f,  0.457f,
-        0.820f,  0.883f,  0.371f,
-        0.982f,  0.099f,  0.879f
+    static const GLfloat gUVBufferData[] = {
+        0.000059f, 1.0f-0.000004f,
+    0.000103f, 1.0f-0.336048f,
+    0.335973f, 1.0f-0.335903f,
+    1.000023f, 1.0f-0.000013f,
+    0.667979f, 1.0f-0.335851f,
+    0.999958f, 1.0f-0.336064f,
+    0.667979f, 1.0f-0.335851f,
+    0.336024f, 1.0f-0.671877f,
+    0.667969f, 1.0f-0.671889f,
+    1.000023f, 1.0f-0.000013f,
+    0.668104f, 1.0f-0.000013f,
+    0.667979f, 1.0f-0.335851f,
+    0.000059f, 1.0f-0.000004f,
+    0.335973f, 1.0f-0.335903f,
+    0.336098f, 1.0f-0.000071f,
+    0.667979f, 1.0f-0.335851f,
+    0.335973f, 1.0f-0.335903f,
+    0.336024f, 1.0f-0.671877f,
+    1.000004f, 1.0f-0.671847f,
+    0.999958f, 1.0f-0.336064f,
+    0.667979f, 1.0f-0.335851f,
+    0.668104f, 1.0f-0.000013f,
+    0.335973f, 1.0f-0.335903f,
+    0.667979f, 1.0f-0.335851f,
+    0.335973f, 1.0f-0.335903f,
+    0.668104f, 1.0f-0.000013f,
+    0.336098f, 1.0f-0.000071f,
+    0.000103f, 1.0f-0.336048f,
+    0.000004f, 1.0f-0.671870f,
+    0.336024f, 1.0f-0.671877f,
+    0.000103f, 1.0f-0.336048f,
+    0.336024f, 1.0f-0.671877f,
+    0.335973f, 1.0f-0.335903f,
+    0.667969f, 1.0f-0.671889f,
+    1.000004f, 1.0f-0.671847f,
+    0.667979f, 1.0f-0.335851f
     };
 
-    GLuint colorBuffer;
-    glGenBuffers(1, &colorBuffer);
-    glBindBuffer(GL_ARRAY_BUFFER, colorBuffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(gColorBufferData), gColorBufferData, GL_STATIC_DRAW);
+    GLuint uvBuffer;
+    glGenBuffers(1, &uvBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, uvBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(gUVBufferData), gUVBufferData, GL_STATIC_DRAW);
 
     std::optional<GLuint> programIDOptional = loadShaders("simpleShader.vert", "simpleShader.frag");
 
@@ -168,7 +169,7 @@ int main()
 
     //Матрица камеры
     glm::mat4 viewMat4 = glm::lookAt(
-        glm::vec3(4, 3, -3), // Координаты камеры в мировом пространстве
+        glm::vec3(4, 3, 3), // Координаты камеры в мировом пространстве
         glm::vec3(0, 0, 0), // и куда смотрит
         glm::vec3(0, 1, 0)  // Видимо с какой стороны находится верх камеры (0,-1,0 для вида вверх ногами)
     );
@@ -181,6 +182,12 @@ int main()
 
     // Получение id для матрицы MVP в шейдерной программе
     GLuint mvpMatID = glGetUniformLocation(programID, "MVP");
+
+     // Загрузка текстуры
+    GLuint texture = loadBMP_custom("uvtemplate.bmp");
+
+    // Получение id для myTextureSampler
+    GLuint textureID = glGetUniformLocation(programID, "myTextureSampler");
 
     // Установка размера окна просмотра OpenGL
     glViewport(0, 0, WIDTH, HEIGHT);
@@ -195,6 +202,12 @@ int main()
         // Отправка матрицы текущему привязаному шейдеру в MVP
         glUniformMatrix4fv(mvpMatID, 1, GL_FALSE, glm::value_ptr(mvp)); // последний аргумент указатель на массив
 
+        // Привзяка текстуры к Texture Unit 0
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        // Установка myTextureSampler для использования в Texture Unit 0
+        glUniform1i(textureID, 0);
+
         glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
         // Первый буффер аттрибутов - вершины  
         glVertexAttribPointer(
@@ -207,8 +220,8 @@ int main()
         );
         glEnableVertexAttribArray(0);
 
-        glBindBuffer(GL_ARRAY_BUFFER, colorBuffer);
-        // Второй буффер аттрибутов - цвета
+        glBindBuffer(GL_ARRAY_BUFFER, uvBuffer);
+        // Второй буффер аттрибутов - UV координаты
         glVertexAttribPointer(
             1, // значение для layout в шейдере
             3, // размер аттрибута. В данном случае вершины, которая состоит из 3х координат
@@ -228,4 +241,14 @@ int main()
         glfwPollEvents();
 
     } while(glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(window) == 0); // Проверяем Escape на нажатие, или пользователь вручную закрыл окно
+
+    // Очистка VBO и шейдера
+    glDeleteBuffers(1, &vertexBuffer);
+    glDeleteBuffers(1, &uvBuffer);
+    glDeleteProgram(programID);
+    glDeleteTextures(1, &texture);
+    glDeleteVertexArrays(1, &vertexArrayID);
+
+    // Закрыть OpenGL окно и завершить GLFW
+    glfwTerminate();
 }
